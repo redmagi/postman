a1032b69836073063abbde90f338772df92552cd
Pitt Phunsanit <phunsanit@hotmail.com>
2018-03-18 19:58:43 +0700

    first init laravel & vuejs

---
+++ resources/views/header.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Pitt Phunsanit">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="post depository system">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="../../../../favicon.ico" rel="icon">
+
+    <title>Postman: post depository system</title>
+</head>
+
+<body>
+    <main class="container" role="main">
